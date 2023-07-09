80c0f568b0242d665f771dabd1a75e0b59e6f112
xup60521 <84887662+xup60521@users.noreply.github.com>
2023-07-09 20:20:46 +0800

    add color to item

--- src/pages/home/components/item.js
+++ src/pages/home/components/item.js
@@ -1,3 +1,5 @@
+import seedrandom, { alea, xor128, xor4096 } from "seedrandom";
+
 const Item = ({id, note, date, title, setData, rerenderStatus}) => {
 
     function deleteItem() {
@@ -7,14 +9,18 @@ const Item = ({id, note, date, title, setData, rerenderStatus}) => {
         rerenderStatus.current = true;
     }
     
+    const rng = new xor4096(date);
+    const randomColor = Math.floor(rng()*16777215).toString(16).padStart(6, "0");
+    const blackandwhite = 0.299*parseInt(randomColor.substring(0,2), 16)+0.587*parseInt(randomColor.substring(2,4), 16)+0.114*parseInt(randomColor.substring(4,6), 16)
+    
+  
     return (
-        <div className="item">
-            <div className="content">
+        <div className="item" style={{backgroundColor: "#"+randomColor }}>
+            <div className="content" style={{color: (blackandwhite > 128) ? "black" : "white"}}>
                 <p className="item title">{title}</p>
                 <p className="item date">{date}</p>
                 <p className="item note">{note}</p>
             </div>
-            
             <button onClick={deleteItem} className="delete">刪除</button>
             
             
